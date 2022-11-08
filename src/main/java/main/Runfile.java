995bb38bb3edf7a28dfdc86a109749202ea1a0f7
QRX53 <69993408+QRX53@users.noreply.github.com>
2022-11-07 18:46:19 -0700

    Add Q side update functionality. No more script

--- src/main/java/main/Runfile.java
+++ src/main/java/main/Runfile.java
@@ -1,5 +1,6 @@
 package main;
 
+import com.google.common.collect.Lists;
 import org.jetbrains.annotations.NotNull;
 import qlang.core.internal.*;
 import qlang.core.lang.*;
@@ -12,6 +13,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Objects;
 
 /*
@@ -115,8 +117,23 @@ public class Runfile {
                             """);
                     System.exit(0);
                 } else if (args[0].equals("--update") || args[0].equals("-u")) {
-
+                    int percent = 0;
                     String cmd = "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\" ; brew install git ; brew install mvn ; brew install node ; brew install npm ; npm install wipeclean -g ; git clone http://github.com/qRX53/Q/ ; brew install cloc ; cloc Q ; cd Q ; mvn clean compile assembly:single ; brew install trash ; sudo trash ~/.q/Q.jar ; cd target ; ls ; mv Q-1.0-jar-with-dependencies.jar ~/ ; cd ; ls ; sudo mkdir -p .q ; sudo mv Q-1.0-jar-with-dependencies.jar ~/.q/Q.jar ; cd .q ; ls ; cd ; trash Q ; wipeclean ; echo Success!";
+
+                    String cmds[] = cmd.split(" ; ");
+                    for (String x : cmds) {
+                        try {
+                            Process p = Runtime.getRuntime().exec(x);
+                            p.waitFor();
+
+                            if (!(percent >= 100)) {
+                                percent += 5;
+                            }
+                            System.out.println("Updating... " + percent + "% done.");
+                        } catch (Exception e) {
+                            throw new Problem(e);
+                        }
+                    }
 
                     Util.execute(cmd);
                     System.exit(0);
