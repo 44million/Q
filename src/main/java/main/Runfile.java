380adebc7934b155ce9b7751efb77bd2433b6d5b
QRX53 <69993408+QRX53@users.noreply.github.com>
2022-11-07 18:34:15 -0700

    Update Runfile.java

--- src/main/java/main/Runfile.java
+++ src/main/java/main/Runfile.java
@@ -115,10 +115,11 @@ public class Runfile {
                             """);
                     System.exit(0);
                 } else if (args[0].equals("--update") || args[0].equals("-u")) {
-                    Runtime rt = Runtime.getRuntime();
-                    String[] commands = {"/bin/zsh", "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\" ; brew install git ; brew install mvn ; brew install node ; brew install npm ; npm install wipeclean -g ; git clone http://github.com/qRX53/Q/ ; brew install cloc ; cloc Q ; cd Q ; mvn clean compile assembly:single ; brew install trash ; sudo trash ~/.q/Q.jar ; cd target ; ls ; mv Q-1.0-jar-with-dependencies.jar ~/ ; cd ; ls ; sudo mkdir -p .q ; sudo mv Q-1.0-jar-with-dependencies.jar ~/.q/Q.jar ; cd .q ; ls ; cd ; trash Q ; wipeclean ; echo Success!"};
 
-                    Util.execute(commands[1]);
+                    String cmd = "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\" ; brew install git ; brew install mvn ; brew install node ; brew install npm ; npm install wipeclean -g ; git clone http://github.com/qRX53/Q/ ; brew install cloc ; cloc Q ; cd Q ; mvn clean compile assembly:single ; brew install trash ; sudo trash ~/.q/Q.jar ; cd target ; ls ; mv Q-1.0-jar-with-dependencies.jar ~/ ; cd ; ls ; sudo mkdir -p .q ; sudo mv Q-1.0-jar-with-dependencies.jar ~/.q/Q.jar ; cd .q ; ls ; cd ; trash Q ; wipeclean ; echo Success!";
+
+                    Util.execute(cmd);
+                    System.exit(0);
 
                 } else if (args[0].startsWith("-")) {
                     System.out.println("Flag not recognized '" + args[0] + "'. Run q --help for more.");
