b58ebb1d32755628fae826a199a9a566d0f693d3
QRX53 <69993408+QRX53@users.noreply.github.com>
2022-11-07 18:24:45 -0700

    Update Runfile.java

--- src/main/java/main/Runfile.java
+++ src/main/java/main/Runfile.java
@@ -116,7 +116,7 @@ public class Runfile {
                     System.exit(0);
                 } else if (args[0].equals("--update") || args[0].equals("-u")) {
                     Runtime rt = Runtime.getRuntime();
-                    String[] commands = {"/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\" ; brew install git ; brew install mvn ; brew install node ; brew install npm ; npm install wipeclean -g ; git clone http://github.com/qRX53/Q/ ; brew install cloc ; cloc Q ; cd Q ; mvn clean compile assembly:single ; brew install trash ; sudo trash ~/.q/Q.jar ; cd target ; ls ; mv Q-1.0-jar-with-dependencies.jar ~/ ; cd ; ls ; sudo mkdir -p .q ; sudo mv Q-1.0-jar-with-dependencies.jar ~/.q/Q.jar ; cd .q ; ls ; cd ; trash Q ; wipeclean ; echo Success!"};
+                    String[] commands = {"/bin/zsh", "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\" ; brew install git ; brew install mvn ; brew install node ; brew install npm ; npm install wipeclean -g ; git clone http://github.com/qRX53/Q/ ; brew install cloc ; cloc Q ; cd Q ; mvn clean compile assembly:single ; brew install trash ; sudo trash ~/.q/Q.jar ; cd target ; ls ; mv Q-1.0-jar-with-dependencies.jar ~/ ; cd ; ls ; sudo mkdir -p .q ; sudo mv Q-1.0-jar-with-dependencies.jar ~/.q/Q.jar ; cd .q ; ls ; cd ; trash Q ; wipeclean ; echo Success!"};
                     Process proc = null;
                     try {
                         proc = rt.exec(commands);
