a210d23052e09d620bc81604a115c80ee6022cd6
QRX53 <69993408+QRX53@users.noreply.github.com>
2022-11-09 13:12:27 -0700

    Update Runfile.java

--- src/main/java/main/Runfile.java
+++ src/main/java/main/Runfile.java
@@ -75,6 +75,7 @@ public class Runfile {
                             --terminal (-t) | Allows you to enter your code in the terminal directly, rather than a file.
                             --help (-h) | Returns this menu.
                             --version (-v) | Get the Q version. Good for checking installation status.
+                            --interact (-in) | interact with the Q CLI
                             """);
                     System.exit(0);
                 } else if (args[0].equals("-i") || args[0].equals("--info")) {
