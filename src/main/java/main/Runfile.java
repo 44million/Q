8af30358c7e62ed9f2b6589aeffa714deb6b576f
QRX53 <69993408+QRX53@users.noreply.github.com>
2022-11-09 13:22:14 -0700

    Update Runfile.java

--- src/main/java/main/Runfile.java
+++ src/main/java/main/Runfile.java
@@ -123,8 +123,7 @@ public class Runfile {
                             """);
                     System.exit(0);
                 } else if (args[0].equals("--interact") || args[0].equals("-in")) {
-		    System.out.println("Interactive entered. 'h' for help, 
-'q' to quit");	
+		    System.out.println("Interactive entered. 'h' for help, 'q' to quit");
                     String input = "";
 
                     while (true) {
