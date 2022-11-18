bed467e943a1ed435caddd7bdee812917445a259
QRX53 <69993408+QRX53@users.noreply.github.com>
2022-11-18 08:47:36 -0700

    Update

--- src/main/java/qlang/core/lang/Util.java
+++ src/main/java/qlang/core/lang/Util.java
@@ -25,81 +25,85 @@ public class Util {
 
     public static String updateScript =
             """
-                                
-                            #!/bin/bash
+                    #!/bin/bash
                                         
-                            # define silly colors for figlet
-                            GREEN='\\033[0;32m'
-                            NC='\\033[0m'
-                            ALIASQ='alias q'
-                            clear ;
-                            # set color to green
-                            echo "\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n${GREEN}"
+                    # define silly colors for figlet
+                    GREEN='\\033[0;32m'
+                    NC='\\033[0m'
+                    ALIASQ='alias q'
+                    clear
+                    # set color to green
+                    echo -e "\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n${GREEN}"
                                         
-                            echo "Beginning install process. This will take a while, and will require sudo access. Please allow up to 5 minutes"
+                    echo "Beginning install process. This will take a while, and will require sudo access. Please allow up to 5 minutes"
                                         
-                            # leave green coloration.
-                            echo "${NC}"
+                    # leave green coloration.
+                    echo "${NC}"
                                         
-                            sleep 7 ;
+                    sleep 7
                                         
-                            # install brew, just in case user doesnt have it already
-                            # shellcheck disable=SC2164
-                            cd ; /bin/bash -c "$(curl -fsSL -s https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" ;
-                            # install git, just in case user doesnt have it already
-                            brew install git ;
-                            # install mvn, just in case user doesnt have it already
-                            brew install mvn ;
-                            # install node, just in case user doesnt have it already
-                            brew install node ;
-                            # install npm, just in case user doesnt have it already
-                            brew install npm ;
-                            # install java, just in case user doesnt have it already
-                            brew install java;
-                            # install figlet, just in case user doesnt have it already
-                            brew install figlet ;
-                            # clone the repo into a new folder
-                            git clone http://github.com/qRX53/Q/ QLANGUPDATEFOLDERTEMP ; cd QLANGUPDATEFOLDERTEMP ;
-                            # assemble the jarfile (with dependencies)
-                            mvn clean compile assembly:single -q ;
-                            # install trash, just in case user doesnt have it already
-                            brew install trash ;\s
-                            # move the old Q jarfile into the trash.
-                            sudo trash ~/.q/Q.jar ;
-                            # change into the target folder, and then move the new jarfile into the home dir
-                            cd target || exit ; mv Q-1.0-jar-with-dependencies.jar ~/ ;
-                            # cd to the home dir, make the .q folder if there isnt one already
-                            cd || exit ; sudo mkdir -p .q ;
-                            # move the new jarfile into the .q folder, and rename it.
-                            sudo mv Q-1.0-jar-with-dependencies.jar ~/.q/Q.jar ;
-                            # move the cloned repo to the trash
-                            trash QLANGUPDATEFOLDERTEMP ;
-                            # clear the console
-                            clear ;
+                    # install brew, just in case user doesnt have it already
+                    # shellcheck disable=SC2164
+                    cd
+                    /bin/bash -c "$(curl -fsSL -s https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+                    # install git, just in case user doesnt have it already
+                    brew install -q git
+                    # install mvn, just in case user doesnt have it already
+                    brew install mvn -q
+                    # install node, just in case user doesnt have it already
+                    brew install node -q
+                    # install npm, just in case user doesnt have it already
+                    brew install npm -q
+                    # install java, just in case user doesnt have it already
+                    brew install java -q
+                    # install figlet, just in case user doesnt have it already
+                    brew install figlet -q
+                    # clone the repo into a new folder
+                    git clone http://github.com/qRX53/Q/ QLANGUPDATEFOLDERTEMP &>/dev/null
+                    cd QLANGUPDATEFOLDERTEMP || exit
+                    # assemble the jarfile (with dependencies)
+                    mvn clean compile assembly:single -q
+                    # install trash, just in case user doesnt have it already
+                    brew install trash -q
+                    # move the old Q jarfile into the trash.
+                    sudo trash ~/.q/Q.jar
+                    # change into the target folder, and then move the new jarfile into the home dir
+                    cd target || exit
+                    mv Q-1.0-jar-with-dependencies.jar ~/
+                    # cd to the home dir, make the .q folder if there isnt one already
+                    cd || exit
+                    sudo mkdir -p .q
+                    # move the new jarfile into the .q folder, and rename it.
+                    sudo mv Q-1.0-jar-with-dependencies.jar ~/.q/Q.jar
+                    # move the cloned repo to the trash
+                    trash QLANGUPDATEFOLDERTEMP
+                    # clear the console
+                    clear
                                         
-                            if ! sudo grep -q "${ALIASQ}" "$~/.zshrc"; then
-                            	sudo echo "alias q='java -jar ~/.q/Q.jar'" >> ~/.zshrc
-                            fi
+                    if ! sudo grep -q "${ALIASQ}" "$~/.zshrc"; then
+                      # shellcheck disable=SC2024
+                      sudo echo "alias q='java -jar ~/.q/Q.jar'" >>~/.zshrc
+                    fi
                                         
-                            if ! sudo grep -q "${ALIASQ}" "$~/.bashrc"; then
-                            	sudo echo "alias q='java -jar ~/.q/Q.jar'" >> ~/.bashrc
-                            fi
+                    if ! sudo grep -q "${ALIASQ}" "$~/.bashrc"; then
+                      # shellcheck disable=SC2024
+                      sudo echo "alias q='java -jar ~/.q/Q.jar'" >>~/.bashrc
+                    fi
                                         
-                            clear ;
+                    clear
                                         
-                            # set color to green
-                            echo "${GREEN}"
+                    # set color to green
+                    echo "${GREEN}"
                                         
-                            # create ansi 'success' text
-                            figlet "Success!"
+                    # create ansi 'success' text
+                    figlet "Success!"
                                         
-                            # leave green coloration.
-                            echo "${NC}"
+                    # leave green coloration.
+                    echo "${NC}"
                                         
-                            echo "Run 'q -v' to verify installation"
-                            # simple as.
+                    echo "Run 'q -v' to verify installation"
+                    # simple as.
                                         
-                                
                     """;
 
     public final static void clearConsole() {
