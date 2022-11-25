9c82a98296a5a8230cd81113e21484f67a92c663
QRX53 <69993408+QRX53@users.noreply.github.com>
2022-11-24 21:06:19 -0700

    Update Visitor.java

--- src/main/java/qlang/core/lang/Visitor.java
+++ src/main/java/qlang/core/lang/Visitor.java
@@ -1833,7 +1833,7 @@ public class Visitor extends QBaseVisitor<Value> implements Cloneable {
                 String afterClass = Util.replaceLast(createdFile.getName(), ".java", ".class");
 
                 Runtime rt = Runtime.getRuntime();
-                String[] commands = {"javac " + createdFile.getName() + " ; java " + afterClass};
+                String[] commands = {"javac " + createdFile.getName() + " ; java " + className};
                 Process proc = rt.exec(commands);
 
                 BufferedReader stdInput = new BufferedReader(new
